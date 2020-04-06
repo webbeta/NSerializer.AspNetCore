f98c87441dcd95245acd5026184d75d327ea4861
Moisés Gramary <moigramary@gmail.com>
2020-04-06 19:56:43 +0200

    Add sample project

---
+++ webBeta.NSerializer.AspNetCore.Sample/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webBeta.NSerializer.AspNetCore.Sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly NSerializer _serializer;
+
+        public DemoController(NSerializer serializer)
+        {
+            _serializer = serializer;
+        }
+
+        [HttpGet("ascreated")]
+        public ActionResult AsCreated()
+        {
+            return _serializer.SerializeAndCreated(new Demo(), "created_group");
+        }
+
+        [HttpGet("asok")]
+        public ActionResult AsOk()
+        {
+            return _serializer.SerializeAndCreated(new Demo(), "ok_group");
+        }
+    }
+}
---
+++ webBeta.NSerializer.AspNetCore.Sample/Demo.cs
@@ -0,0 +1,8 @@
+namespace webBeta.NSerializer.AspNetCore.Sample
+{
+    public class Demo
+    {
+        public string CreatedMessage { get; } = "Created message!";
+        public string OkMessage { get; } = "Ok message!";
+    }
+}
